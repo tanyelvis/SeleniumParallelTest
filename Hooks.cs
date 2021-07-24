6b21743577c1e27b0c4c4fd0209607e6cef33392
Elvis <tanyi.elvis10@gmail.com>
2021-07-24 12:07:01 +0200

    Parallel Selenium test execution with NUnit and Csharp

--- Hooks.cs
+++ Hooks.cs
@@ -1,21 +1,44 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System;
 
 
 namespace SeleniumParallelTest2
 {
+    public enum BrowerType
+    {
+        Chrome,
+        Firefox,
+        IE
+    }
 
     [TestFixture]
     // initialising the driver object
     public class Hooks : Base
     {
-        public Hooks()
+        private BrowerType _browserType;
+
+        public Hooks(BrowerType browser)
         {
-            Driver = new FirefoxDriver();
-
-
+            //Driver = new FirefoxDriver();
+            _browserType = browser;
         }
 
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            ChooseDriverInstance(_browserType);
+        }
+        public void ChooseDriverInstance(BrowerType browerType)
+        {
+            if (browerType == BrowerType.Chrome)
+                Driver = new ChromeDriver();
+            else if (browerType == BrowerType.Firefox)
+                Driver = new FirefoxDriver();
+           // else if (browerType == BrowerType.IE)
+             //   Driver = new IEDriver();
+        }
     }
 }
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,8 +7,14 @@ using OpenQA.Selenium.Interactions;
 namespace SeleniumParallelTest2 
 {
     [TestFixture]
+    [Parallelizable]
     public class FirefoxTesting  : Hooks
     {
+
+        public FirefoxTesting() : base(BrowerType.Firefox)
+        {
+
+        }
        
 
         [Test]
@@ -19,8 +25,8 @@ namespace SeleniumParallelTest2
             Driver.FindElement(By.Name("q")).SendKeys("Selenium");
             Driver.FindElement(By.Name("btnG")).Click();
             //Thread.Sleep(50);
-           
 
+            System.Threading.Thread.Sleep(20000);
             Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true), 
                                         "The text Selenium does not exist");
 
@@ -32,8 +38,13 @@ namespace SeleniumParallelTest2
 
 
     [TestFixture]
+    [Parallelizable]
     public class ChromeTesting : Hooks
     {
+        public ChromeTesting() : base(BrowerType.Chrome)
+        {
+        }
+
         [Test]
         public void ChromeGoogleTest()
         {
@@ -41,6 +52,8 @@ namespace SeleniumParallelTest2
             Driver.FindElement(By.Id("L2AGLb")).Click();
             Driver.FindElement(By.Name("q")).SendKeys("ExecuteAutomation");
             Driver.FindElement(By.Name("btnG")).Click();
+            System.Threading.Thread.Sleep(20000);
+
             Assert.That(Driver.PageSource.Contains("ExecuteAutomation"), Is.EqualTo(true),
                                         "The text ExecuteAutomation does not exist");
 
@@ -49,5 +62,32 @@ namespace SeleniumParallelTest2
 
         
     }
+
+
+   // [TestFixture]
+    //[Parallelizable]
+    //public class IETesting : Hooks
+   // {
+     //   public IETesting(BrowerType browser) : base(browser)
+       // {
+        //}
+
+        //[Test]
+        //public void IEGoogleTest()
+       // {
+         //   Driver.Navigate().GoToUrl("https://www.google.com/");
+           // Driver.FindElement(By.Id("L2AGLb")).Click();
+            //Driver.FindElement(By.Name("q")).SendKeys("ExecuteAutomation");
+            //Driver.FindElement(By.Name("btnG")).Click();
+            //System.Threading.Thread.Sleep(20000);
+
+            //Assert.That(Driver.PageSource.Contains("ExecuteAutomation"), Is.EqualTo(true),
+               //                         "The text ExecuteAutomation does not exist");
+    //
+
+      //  }
+
+
+    //}
 
 }
